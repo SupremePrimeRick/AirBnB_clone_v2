f63ac0630f774e198a9c2de94ba0aa96eff22152
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-06 22:27:18 +0100

    updated task 6

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -6,18 +6,18 @@ import unittest
 import MySQLdb
 
 
-@unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'db_storage tests not supported in file storage')
-
+@unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db',
+                 'db_storage tests not supported in file storage')
 class TestDBStorage(unittest.TestCase):
     """ Test class """
 
     def test_new_and_save(self):
         """ Testing the new and save methods"""
         db = MySQLdb.connect(user=getenv('HBNB_MYSQL_USER'),
-                            passwd=getenv('HBNB_MYSQL_PWD'),
-                            host=getenv('HBNB_MYSQL_HOST'),
-                            db=getenv('HBNB_MYSQL_DB'),
-                            port=3306)
+                             passwd=getenv('HBNB_MYSQL_PWD'),
+                             host=getenv('HBNB_MYSQL_HOST'),
+                             db=getenv('HBNB_MYSQL_DB'),
+                             port=3306)
         new_state = State(name='Nigeria')
         cur = db.cursor()
         cur.execute('SELECT COUNT(*) FROM states')
@@ -25,12 +25,12 @@ class TestDBStorage(unittest.TestCase):
         cur.close()
         db.close()
         new_state.save()
-        
+
         db = MySQLdb.connect(user=getenv('HBNB_MYSQL_USER'),
-                            passwd=getenv('HBNB_MYSQL_PWD'),
-                            host=getenv('HBNB_MYSQL_HOST'),
-                            db=getenv('HBNB_MYSQL_DB'),
-                            port=3306)
+                             passwd=getenv('HBNB_MYSQL_PWD'),
+                             host=getenv('HBNB_MYSQL_HOST'),
+                             db=getenv('HBNB_MYSQL_DB'),
+                             port=3306)
         cur = db.cursor()
         cur.execute('SELECT COUNT(*) FROM states')
         new_count = cur.fetchall()
@@ -38,4 +38,3 @@ class TestDBStorage(unittest.TestCase):
         self.assertEqual(new_count[0][0], old_count[0][0] + 1)
         cur.close()
         db.close()
-       
