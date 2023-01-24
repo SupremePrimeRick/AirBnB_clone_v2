c2fd32adcec740605a2ebf6f6a2687b51aede372
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-24 11:29:42 +0100

    Full web

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive from contents of web_static"""
-from fabric.api import local, env, put, run, runs_once
+from fabric.api import local, env, put, run, runs_once, sudo
 from datetime import datetime
 import os
 
-env.hosts = ['100.26.233.107', '18.204.8.41']
+env.hosts = ['ubuntu@100.26.233.107', 'ubuntu@18.204.8.41']
+
 
 @runs_once
 def do_pack():
@@ -18,6 +19,7 @@ def do_pack():
         return None
     return path
 
+
 def do_deploy(archive_path):
     """Deploys static files to the host servers
     Args:
@@ -28,24 +30,24 @@ def do_deploy(archive_path):
         return False
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}".format(folder_name)
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
     try:
         put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        sudo("mkdir -p {}".format(folder_path))
+        sudo("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        sudo("rm -rf /tmp/{}".format(file_name))
+        sudo("mv {}web_static/* {}".format(folder_path, folder_path))
+        sudo("rm -rf {}web_static".format(folder_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {} /data/web_static/current".format(folder_path))
         print("New version is now LIVE!")
         success = True
     except Exception:
         success = False
     return success
 
-    def deploy():
-        """Archives and deploys the static files to host servers"""
-        archive_path = do_pack()
-        return do_deploy(archive_path) if archive_path else Fals
 
+def deploy():
+    """Archives and deploys the static files to host servers"""
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
