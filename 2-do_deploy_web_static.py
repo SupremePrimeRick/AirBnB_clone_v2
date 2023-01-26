2b064d833bb27da4f054a3d03fec60825183bf6b
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-01-26 16:50:02 -0600

    hotfix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,56 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from contents of web_static"""
-from fabric.api import local, env, put, run, runs_once, sudo
+"""Compress web static package
+"""
+from fabric.api import *
 from datetime import datetime
-import os
-
-env.hosts = ['ubuntu@100.26.233.107', 'ubuntu@18.204.8.41']
+from os import path
 
 
-@runs_once
-def do_pack():
-    """creates a tar file"""
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz".format(
-        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static".format(path))
-
-    if result.failed:
-        return None
-    return path
+env.hosts = ['54.86.220.207', '54.175.137.217']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """Deploys static files to the host servers
-    Args:
-        archive_path (str): path to archived static file
-    """
+        """Deploy web files to server
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
 
-    if not os.path.exists(archive_path):
-        return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        sudo("mkdir -p {}".format(folder_path))
-        sudo("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        sudo("rm -rf /tmp/{}".format(file_name))
-        sudo("mv {}web_static/* {}".format(folder_path, folder_path))
-        sudo("rm -rf {}web_static".format(folder_path))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {} /data/web_static/current".format(folder_path))
-        print("New version is now LIVE!")
-        success = True
-    except Exception:
-        success = False
-    return success
+                # upload archive
+                put(archive_path, '/tmp/')
+
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
+
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
+
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
+
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
+
+        # return True on success
+        return True
